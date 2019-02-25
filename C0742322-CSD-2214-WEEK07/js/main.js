80642dcf788b432cffa0a5780130d07cd1932804
keerthiRAV <keerthiraveendran93@gmail.com>
2019-02-25 14:48:22 -0500

    commit

--- C0742322-CSD-2214-WEEK07/js/main.js
+++ C0742322-CSD-2214-WEEK07/js/main.js
@@ -1,1 +1,10 @@
 ﻿// Your code here!
+function add() {
+    var sum = 0;
+    for (i = 117; i >= 11; i = i - 8) {
+        document.writeln(i);
+        //document.getElementById('s').innerHTML = i;
+        sum = sum + i;
+    }
+    alert("sum is " + sum);
+}
